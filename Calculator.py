bdaa5eeedb372c0c3fbfed14dd342c5cb82baeba
Omri Markovich <Omri.markovich@mail.huji.ac.il>
2025-06-13 15:57:38 +0300

    pep 8 format

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,6 @@
-
 from abc import ABC, abstractmethod
-from math import sin, cos, tan, log , radians
-from typing import Any , Optional , Union
-
+from math import sin, cos, tan, log, radians
+from typing import Any, Optional, Union
 
 # Constants for actions
 
@@ -15,77 +13,80 @@ EXIT_MESSAGE = "Exiting the calculator.\n"
 CONTINUE_MESSAGE = "Press Enter to continue or 'e' to exit: \n"
 CONFIRMATION_MESSAGE = "Confirm your input. Write 'yes' to confirm \n"
 
-class SimpleCalculator:
 
+class SimpleCalculator:
     EXCEPTION_MESSAGE = "An error occurred during th calculation. return the last result: \n"
 
     def __init__(self):
         self.__last_result = None
 
-    def calculate(self, action_name : str,  *args : Optional[str]) -> Any:
+    def calculate(self, action_name: str, *args: Optional[str]) -> Any:
         try:
             action, action_type = ActionsFactory.get_action(action_name)
             self.input_check(*args)
-            args = self.add_last_result(action_type ,*args)
+            args = self.add_last_result(action_type, *args)
             self.__last_result = action(*args)
-        except (ValueError , TypeError , ZeroDivisionError)  as e:
-            print(e ,"\n", self.EXCEPTION_MESSAGE)
+        except (ValueError, TypeError, ZeroDivisionError) as e:
+            print(e, "\n", self.EXCEPTION_MESSAGE)
         except Exception as e:
-            print(f"An unexpected error occurred: {e}" , "\n", self.EXCEPTION_MESSAGE)
+            print(f"An unexpected error occurred: {e}", "\n", self.EXCEPTION_MESSAGE)
         finally:
             return self.get_last_result()
 
     def get_last_result(self):
         if self.__last_result is None:
-                print("No calculation has been performed yet \n")
-                return None
+            print("No calculation has been performed yet \n")
+            return None
         return self.__last_result
 
     def input_check(self, *args: Optional[str]) -> None:
         for arg in args:
-            if  arg.strip().lower()  != "none" and not arg.isnumeric() and not(self.is_negative(arg  )):
+            if arg.strip().lower() != "none" and not arg.isnumeric() and not (
+                    self.is_negative(arg)):
                 raise TypeError(f"Unary actions require numeric arguments. Invalid argument: {arg}")
 
     @classmethod
-    def is_negative(cls, arg : str ) -> bool:
+    def is_negative(cls, arg: str) -> bool:
         return True if arg[0] == "-" and arg[1:].isnumeric() else False
 
-    def add_last_result(self, action_type : bool , *args: Optional[str]) -> tuple[Optional[str]]:
+    def add_last_result(self, action_type: bool, *args: Optional[str]) -> tuple[Optional[str]]:
         min_argument = 0 if action_type else 1
-        if len(args) == min_argument  :
-            args = (str(self.get_last_result()) ,) + args
+        if len(args) == min_argument:
+            args = (str(self.get_last_result()),) + args
 
-        elif args[0].strip().lower() ==  "none":
-            args = (str(self.get_last_result()) ,) + args[1:]
+        elif args[0].strip().lower() == "none":
+            args = (str(self.get_last_result()),) + args[1:]
         return args
 
 
 class BinaryAction(ABC):
     MIN_ARGUMENTS = 2
+
     @abstractmethod
-    def __call__(self, *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         raise NotImplementedError("This method should be overridden by subclasses")
 
     @classmethod
-    def argument_check(cls ,  *args : Optional[str]) -> None:
+    def argument_check(cls, *args: Optional[str]) -> None:
         if len(args) < cls.MIN_ARGUMENTS:
             raise ValueError("At least two arguments are required for binary actions")
 
 
 class UnaryActions(ABC):
     MAX_ARGUMENTS = 1
+
     @abstractmethod
     def __call__(self, a: float):
         raise NotImplementedError("This method should be overridden by subclasses")
 
     @classmethod
-    def argument_check(cls ,  *args : Optional[str]) -> None:
+    def argument_check(cls, *args: Optional[str]) -> None:
         if len(args) > cls.MAX_ARGUMENTS:
             raise TypeError("Unary actions require at most 1 argument")
 
 
 class AddAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         ans = 0
         for ind in range(len(args)):
             ans += 0 if args[ind].strip().lower() == "none" else float(args[ind])
@@ -93,7 +94,7 @@ class AddAction(BinaryAction):
 
 
 class SubtractAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         ans = float(args[0])
         for ind in range(1, len(args)):
             ans -= 0 if args[ind].strip().lower() == "none" else float(args[ind])
@@ -101,7 +102,7 @@ class SubtractAction(BinaryAction):
 
 
 class MultiplyAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         ans = 1
         for ind in range(len(args)):
             ans *= 1 if args[ind].strip().lower() == "none" else float(args[ind])
@@ -109,7 +110,7 @@ class MultiplyAction(BinaryAction):
 
 
 class DivideAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
 
         ans = float(args[0])
         for ind in range(1, len(args)):
@@ -120,20 +121,18 @@ class DivideAction(BinaryAction):
         return ans
 
 
-
 class PowerAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         ans = float(args[0])
         for ind in range(1, len(args)):
-            if (-1 < float(args[ind]) < 1 ) and float(args[ind])!= 0:
-                ans = RootAction(ans ,1 /args[ind])
+            if (-1 < float(args[ind]) < 1) and float(args[ind]) != 0:
+                ans = RootAction(ans, 1 / args[ind])
             ans **= 1 if args[ind].strip().lower() == "none" else float(args[ind])
         return ans
 
 
-
 class RootAction(BinaryAction):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         ans = float(args[0])
         if ans < 0:
             raise ValueError("Cannot calculate square root of a negative number")
@@ -146,23 +145,22 @@ class RootAction(BinaryAction):
 
 
 class SinAction(UnaryActions):
-    def __call__(self,   *args : Optional[str]) -> float:
-            return sin(radians(float(args[0])))
-
+    def __call__(self, *args: Optional[str]) -> float:
+        return sin(radians(float(args[0])))
 
 
 class CosAction(UnaryActions):
-    def __call__(self,  *args : Optional[str]) -> float:
-            return cos(radians(float(args[0])))
+    def __call__(self, *args: Optional[str]) -> float:
+        return cos(radians(float(args[0])))
 
 
 class TanAction(UnaryActions):
-    def __call__(self,  *args : Optional[str]) -> float:
+    def __call__(self, *args: Optional[str]) -> float:
         return tan(radians(float(args[0])))
 
 
 class LogAction(UnaryActions):
-    def __call__(self, args_list: list[int| float]) -> float:
+    def __call__(self, args_list: list[int | float]) -> float:
         if args_list[0] <= 0:
             raise ValueError("Logarithm is undefined for non-positive numbers")
         return log(float(args_list[0]))
@@ -183,7 +181,8 @@ class ActionsFactory:
     }
 
     @classmethod
-    def get_action(cls, action_name: str) -> tuple[BinaryAction, int] | tuple[UnaryActions, int] | tuple[None, None]:
+    def get_action(cls, action_name: str) -> tuple[BinaryAction, int] | tuple[UnaryActions, int] | \
+                                             tuple[None, None]:
         if action_name.lower() in cls.actions:
             return cls.actions[action_name.lower()]
         raise ValueError(f"Action {action_name} is not supported.")
@@ -196,21 +195,17 @@ def main(*args):
         if user_input[0].lower() == "e":
             print(EXIT_MESSAGE)
             break
-        print("You entered:", user_input,"\n", )
+        print("You entered:", user_input, "\n", )
         confirmation = input(CONFIRMATION_MESSAGE).strip().lower()
         if confirmation != 'yes':
             print("Input not confirmed, please try again.")
             continue
         print("Calculating...")
-        print ("Your result:\n" , calc.calculate(*user_input))
+        print("Your result:\n", calc.calculate(*user_input))
         user_input = input(CONTINUE_MESSAGE)
         if user_input.lower() == 'e':
             print(EXIT_MESSAGE)
             break
-
-
-
-
 
 
 if __name__ == "__main__":
